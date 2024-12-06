d4850dbc600d0b8bd93ff9a549e1c46ba007e862
Akash-Balaji003 <akashbalaji.tvr@gmail.com>
2024-12-06 19:48:53 +0530

    Working project both iOS and android (Icons not yet added)

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -3,14 +3,17 @@ import React from 'react'
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 type LoginProps = NativeStackScreenProps<RootStackParamList, 'Register'>;
 
 const Register = ({navigation}: LoginProps) => {
     return (
-        <View>
-            <Text>Register</Text>
-        </View>
+        <SafeAreaView>
+            <View>
+                <Text>Register</Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
