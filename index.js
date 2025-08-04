1f006273e2a1bda703a661161703ede895dbb008
Piyush Prakash Wakpaijan <piyushprakashwakpaijan@gmail.com>
2025-08-04 20:09:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,23 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import BackgroundFetch from 'react-native-background-fetch';
+import backgroundTask from './src/tasks/BackgroundTask.ts';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// Headless JS task for background-fetch
+const HeadlessTask = async (event) => {
+  console.log('[BackgroundFetch HeadlessTask] start: ', event);
+
+  try {
+    await backgroundTask(); // your actual background logic
+  } catch (error) {
+    console.error('Headless backgroundTask error:', error);
+  }
+
+  // Required: Signal completion of your task to native code
+  BackgroundFetch.finish(event.taskId);
+};
+
+BackgroundFetch.registerHeadlessTask(HeadlessTask);
